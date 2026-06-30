200b9363036262de611008882004389a841323f7
Lisa <lisahumairoh01@gmail.com>
2026-06-30 13:52:07 +0700

    feat: modul frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,23 @@
 import express from "express";
 import cors from "cors";
-import mahasiswaRoutes from "./routes/mahasiswa.route"
+import mahasiswaRoutes from "./routes/mahasiswa.route";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "API Express CRUD berjalan" });
+  res.json({ message: "Backend Express berjalan" });
 });
 
 app.use("/api/mahasiswa", mahasiswaRoutes);
-app.use("/api/db/mahasiswa", mahasiswaDbRoutes);
 
 export default app;
--- backend/src/routes/mahasiswa-db.route.ts
+++ backend/src/routes/mahasiswa-db.route.ts
@@ -21,4 +21,72 @@ router.get("/", async (req: Request, res: Response) => {
   }
 });
 
+router.get("/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const [rows]: any = await db.execute(
+      "SELECT * FROM mahasiswa WHERE id = ?",
+      [id]
+    );
+
+    if (rows.length === 0) {
+      return res.status(404).json({
+        message: "Mahasiswa tidak ditemukan",
+      });
+    }
+
+    res.json({
+      message: "Detail mahasiswa berhasil diambil",
+      data: rows[0],
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Terjadi kesalahan server",
+    });
+  }
+});
+
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    const { nim, nama, prodi, angkatan } = req.body;
+
+    if (!nim || !nama || !prodi || !angkatan) {
+      return res.status(400).json({
+        message: "NIM, nama, prodi, dan angkatan wajib diisi",
+      });
+    }
+
+    const [result]: any = await db.execute(
+      "INSERT INTO mahasiswa (nim, nama, prodi, angkatan) VALUES (?, ?, ?, ?)",
+      [nim, nama, prodi, angkatan]
+    );
+
+    res.status(201).json({
+      message: "Mahasiswa berhasil ditambahkan",
+      data: {
+        id: result.insertId,
+        nim,
+        nama,
+        prodi,
+        angkatan,
+      },
+    });
+  } catch (error: any) {
+    console.error(error);
+
+    if (error.code === "ER_DUP_ENTRY") {
+      return res.status(400).json({
+        message: "NIM sudah digunakan",
+      });
+    }
+
+    res.status(500).json({
+      message: "Terjadi kesalahan server",
+    });
+  }
+});
+
+
 export default router;
